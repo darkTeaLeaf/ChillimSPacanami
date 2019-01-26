bd584c643ac3a8cd2c1c122303d2cacb4c0795d9
Alexeyzhu <alekseyzhuchkov.tut@gmail.com>
2019-01-27 00:48:59 +0300

    User service

---
+++ src/main/java/com/example/demo/UserService.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService (UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void addUser(User user){
+        userRepository.save(user);
+    }
+}
